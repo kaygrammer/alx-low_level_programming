219e46520c9d8efe141af7318ae05cea69fc3c5e
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2022-12-11 00:36:54 +0100

    task 0

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+/**
+ * size64 - Size of 32 bit system
+ * Description: Size of 32 bit system
+ * Return: Always 0 (Success)
+ */
+
+int size32(void);
+{
+printf("Size of a char: 1 byte(s)\n");
+printf("Size of an int: 4 byte(s)\n");
+printf("Size of a long int: 4 byte(s)\n");
+printf("Size of a long long int: 8 byte(s)\n");
+printf("Size of a float: 4 byte(s)\n");
+}
+
+/**
+ * size64 - Size of 64 bit system
+ * Description: Size of 64 bit system
+ * Return: Always 0 (Success)
+ */
+int size64(void);
+{
+printf("Size of a char: 1 byte(s)\n");
+printf("Size of an int: 4 byte(s)\n");
+printf("Size of a long int: 8 byte(s)\n");
+printf("Size of a long long int: 8 byte(s)\n");
+printf("Size of a float: 4 byte(s)\n");
+}
+return (0);
+
+}
