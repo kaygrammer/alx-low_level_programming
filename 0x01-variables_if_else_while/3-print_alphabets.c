76dfafd5d389056afddd05d11d22a429a8f61d69
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2022-12-11 20:15:32 +0100

    alphabet using putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+for (c = 'A'; c <= 'Z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
