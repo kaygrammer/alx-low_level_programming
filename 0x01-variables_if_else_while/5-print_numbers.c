f69214e4b0d53453769c2503db657d66d8c221d6
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2022-12-11 23:37:48 +0100

    alphabet using putchar

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c;
+for (c = 0; c <= 10; c++)
+printf("%d\n", c);
+return (0);
+}
