15c3d3ffd637bf4fc287c29a709d17a509af0637
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2022-12-11 23:01:22 +0100

    alphabet using putchar

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,10 +12,10 @@ int main(void)
 char c;
 for (c = 'a'; c <= 'z'; c++)
 {
-if (c == 'q' && c == 'e')
+if (c != 'q' && c != 'e')
 {
-continue; }
-putchar(c);
+putchar(c); }
+continue;
 putchar('\n'); }
 return (0);
 }
