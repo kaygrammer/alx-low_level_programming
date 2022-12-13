1f1acfcae449b315daf122714791230334406680
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2022-12-13 10:46:59 +0100

    mmm

---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+int a = 0;
+int b = 0;
+a = 0;
+for (a < b;;)
+{
+printf("%d\n", a++);
+}
+}
