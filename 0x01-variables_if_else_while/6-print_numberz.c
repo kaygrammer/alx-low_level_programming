698c2063f89fd60ffe67092986f4764c434739d3
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2022-12-13 11:15:14 +0100

    mmm.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c;
+for (c = 0; c < 10; c++)
+
+putchar(c);
+
+printf("\n");
+return (0);
+}
