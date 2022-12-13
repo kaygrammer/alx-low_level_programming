2d3fdf824bd49674dcc0015fae9419dc75624766
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2022-12-13 18:47:48 +0100

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+char c;
+int d;
+for (d = 1; d <= 10; d++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+}
+_putchar('\n');
+}
