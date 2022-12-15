c3a45d00c04cf23118161e50d7bfa37f8ec1842c
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2022-12-16 00:35:51 +0100

    print numer

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+int c;
+for (c = 0; c < 10; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
