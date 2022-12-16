c83d000f10eb60cc6510083493fb704e7b4a253f
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2022-12-16 05:22:58 +0100

    print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code
+ *
+ * Return: Always c.
+ */
+
+void print_most_numbers(void)
+{
+int c;
+for (c = 0; c < 10; c++)
+if (c != 2 && c != 4)
+{
+_putchar(c + '0');
+}
+{
+}
+_putchar('\n');
+}
