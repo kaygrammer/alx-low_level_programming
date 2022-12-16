0f2ba2c3e2dc9e9fecbc50175f79d07e69dbf5a7
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2022-12-16 05:15:40 +0100

    print numer

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_numbers - check the code
  *
- * Return: Always 0.
+ * Return: Always c.
  */
 
 void print_numbers(void)
@@ -11,7 +11,7 @@ void print_numbers(void)
 int c;
 for (c = 0; c < 10; c++)
 {
-_putchar(c);
+_putchar(c + '0');
 }
 _putchar('\n');
 }
