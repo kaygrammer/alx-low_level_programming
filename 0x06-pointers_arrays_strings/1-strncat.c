02a67a420bcba5c9fdd9c959e4e4d6979a737ab4
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2022-12-23 10:48:44 +0100

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,12 +6,13 @@
 *
 * @dest: pointer to the destination string
 * @src: pointer to the source string
-*
+* @n: number of elements to copy in src
 * Return: dest.
 *
 */
 
-char *_strncat(char *dest, char *src, int n){
-    strncat(dest, src, n);
-    return (dest);
+char *_strncat(char *dest, char *src, int n)
+{
+strncat(dest, src, n);
+return (dest);
 }
