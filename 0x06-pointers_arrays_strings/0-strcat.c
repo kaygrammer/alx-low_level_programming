c718ad7f19832c2dff1f3d6bda021c8369267a10
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2022-12-23 12:19:26 +0100

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,47 @@
 #include "main.h"
 #include <string.h>
 
-/**
-** _strcat - string concat
-*
-* @dest: pointer to the destination string
-* @src: pointer to the source string
-*
-* Return: dest.
-*
+/*
+* char *_strcat(char *dest, char *src)
+// {
+// size_t dest_length = strlen(dest);
+// size_t i;
+
+// for (i = 0; src[i] != '\0'; i++)
+// {
+// dest[dest_length + i] = src[i];
+// }
+// dest[dest_length + i] = '\0';
+
+// return (dest);
+// }
 */
+
+
+
+/**
+ * _strcat - string concat
+ *
+ * @dest: input
+ * @src: input
+ *
+ * Return: dest.
+ */
+
 
 char *_strcat(char *dest, char *src)
 {
-strcat(dest, src);
+int dlen = 0, i;
+
+while (dest[dlen])
+{
+dlen++;
+}
+for (i = 0; src[i] != 0; i++)
+{
+dest[dlen] = src[i];
+dlen++;
+}
+dest[dlen] = '\0';
 return (dest);
 }
