f9c9187e82a7f8defd2f02c634f4b9fa72a60278
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-01-03 02:16:51 +0100

    program that works similar to strspn function

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "string.h"
+
+/* betty style doc for function strspn goes there */
+/**
+ * _strspn - strspn function(strchr() is used to gets the
+ * length of a prefix substring.
+ * resources - geeksforgeeks.org, coldvault youtube channel,
+ * portfolio course youtube channel.
+ * Description: 'the program that works similar to strchr function
+ * @s: string to be searched
+ * @accept: string to be searched
+ * Return: Always ret (Success)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+size_t ret = 0;
+while (*s && strchr(accept, *s++))
+ret++;
+return (ret);
+}
