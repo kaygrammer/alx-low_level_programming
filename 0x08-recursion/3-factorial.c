e402ddf3b074e346b93afa5d9e9769d96d6e8cf0
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-01-06 00:24:22 +0100

    4

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+ #include "main.h"
+
+/* betty style doc for 0x08-recursion goes there */
+/**
+ * factorial - _puts_recursion
+ * resources - geeksforgeeks.org, coldvault youtube cahannel.
+ * Description: 'the program that works similar to puts"
+ * @n: integer input
+ * Return: Always (Success)
+ */
+
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+if (n == 0)
+{
+(return 1);
+}
+return (n * factorial(n - 1));
+}
