0f12266e31eb367dc1c5906fe27a7869ef5edf77
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-01-11 13:34:11 +0100

    args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - main function
+ * Return: 1 for uppercase and 0 for lowercase
+ * @argc: int input
+ * @argv: char input
+*/
 
 int main(int argc, char *argv[])
 {
-    int i;
-    for (i = 0; i < argc; i++ )
-    {
-    printf("%s\n", argv[i]);
-    }
-    return (0);
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
 }
