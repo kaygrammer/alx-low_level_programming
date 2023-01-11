399ecf41d8d118a2c7e0a50363d492d0f45010eb
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-01-11 13:16:05 +0100

    print number of argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - main function
+ * Return: 1 for uppercase and 0 for lowercase
+ * @argc: int input
+ * @argv: char input
+*/
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
