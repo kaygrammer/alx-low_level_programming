e6649f7fb9fd5d9c838060f4a6a60afa1ae5e226
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-01-13 12:48:36 +0100

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+ * create_array - check for uppercase
+ * Return: 1 for uppercase and 0 for lowercase
+ * @size: positive integer
+ * @c: charcters
+*/
+
+char *create_array(unsigned int size, char c)
+{
+char *array = (char *)malloc(size * sizeof(char));
+unsigned int i;
+if (size == 0)
+return (NULL);
+if (array == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+{
+array[i] = c;
+}
+return (array);
+}
