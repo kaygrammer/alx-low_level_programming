bad34a9e638a2f0f07eb8f3a6c7ce60df326a83b
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-01-15 07:43:12 +0100

    free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free_grid
+ * Return: grid
+ * @grid: first string
+ * @height: second string
+*/
+
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; i++)
+{
+free(grid[1]);
+}
+free(grid);
+}
