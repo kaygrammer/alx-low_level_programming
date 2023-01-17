1417c1339e0aba4b245c3bb27a74488574c59b11
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-01-17 21:11:51 +0100

    dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,11 @@
  * Description: Longer description
  */
 
-struct dog
-{
-char *name;
-float age;
-char *owner;
-};
+typedef struct dog {
+    char *name;
+    float age;
+    char *owner;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
