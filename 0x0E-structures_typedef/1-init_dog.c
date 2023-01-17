b9f0c69983fb8f05dadc1ccd15bdf7c15973b84f
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-01-17 01:19:14 +0100

    struc init

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,6 @@
 #include "main.h"
 #include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * init_dog - check the code
@@ -17,20 +16,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-d->name = malloc(sizeof(char) * (strlen(name) + 1));
-if (d->name == NULL)
-{
-printf("Error allocating memory for dog name.\n");
-exit(1);
-}
-strcpy(d->name, name);
+if (d != NULL)
+d->name = name;
 d->age = age;
-d->owner = malloc(sizeof(char) * (strlen(owner) + 1));
-if (d->owner == NULL)
-{
-printf("Error allocating memory for dog owner.\n");
-free(d->name);
-exit(1);
-}
-strcpy(d->owner, owner);
+d->owner = owner;
 }
