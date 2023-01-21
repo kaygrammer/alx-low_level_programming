18798139efe3ce8dc9b402ce893e9e7aa286cc05
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-01-21 19:12:30 +0100

    print file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("%s\n", __FILE__);
-    return 0;
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
 }
