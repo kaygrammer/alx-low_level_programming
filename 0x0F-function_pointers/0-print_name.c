f48ec39ef1b4b9a0fd1ef03931ab26614ac3b370
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-01-24 14:35:08 +0100

    function to a pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,13 @@
 #include "function_pointers.h"
 
+/**
+ * print_name - print function from a pointer
+ *
+ * @name: name
+ * @f: f
+ * Return: void.
+ */
+
 
 void print_name(char *name, void (*f)(char *))
 {
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,1 +1,10 @@
+#ifndef FUNCTION_POINTER
+#define FUNCTION_POINTER
+
 void print_name(char *name, void (*f)(char *));
+
+#endif
+
+
+
+
