bbc903ff944030f9eb06836e997f46cd95be5a5d
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-01-24 14:42:03 +0100

    function to a pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
  * print_name - print function from a pointer
@@ -11,5 +12,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+if (name != NULL && f != NULL)
 f(name);
 }
